d1a1707d772d3489c826139e622dda7db5b5b25f
Tron <crazynuggets@outlook.com>
2020-09-16 13:07:04 +0300

    Updated

--- [Handler]/cinecam_handler/exports/client.lua
+++ [Handler]/cinecam_handler/exports/client.lua
@@ -2,8 +2,8 @@
 --[[ Resource: Cinematic Camera Handler
      Script: exports: client.lua
      Server: -
-	 Author: Tron
-	 Developer: -
+     Author: Tron
+     Developer: -
      DOC: 13/10/2019 (Tron)
      Desc: Client Sided Exports ]]--
 ----------------------------------------------------------------
@@ -27,23 +27,23 @@ local _screenSource = DxScreenSource(sX*1366, sY*768)
 
 local function previewCinemation()
 
-	local interpolationStatus = getInterpolationStatus()
-	if not interpolationStatus then
-		Camera.fade(true)
-		if not _customCinemationPoint then
-			if (#availableCinemationPoints) > 0 then
-				startCameraMovement(availableCinemationPoints[math.random(#availableCinemationPoints)])
-			end
-		else
-			if not _customCinemationLoop then
-				_customCinemationLoop = -1
-			elseif _customCinemationLoop == -1 then
-				stopCinemation()
-				return false
-			end
-			startCameraMovement(_customCinemationPoint)
-		end
-	end
+    local interpolationStatus = getInterpolationStatus()
+    if not interpolationStatus then
+        Camera.fade(true)
+        if not _customCinemationPoint then
+            if (#availableCinemationPoints) > 0 then
+                startCameraMovement(availableCinemationPoints[math.random(#availableCinemationPoints)])
+            end
+        else
+            if not _customCinemationLoop then
+                _customCinemationLoop = -1
+            elseif _customCinemationLoop == -1 then
+                stopCinemation()
+                return false
+            end
+            startCameraMovement(_customCinemationPoint)
+        end
+    end
 
     dxUpdateScreenSource(_screenSource)
     if _cinemationBlur and _blurShader and isElement(_blurShader) then
@@ -53,7 +53,7 @@ local function previewCinemation()
         dxDrawImage(0, 0, sX*1366, sY*768, _blurShader)
     else
         dxDrawImage(0, 0, sX*1366, sY*768, _screenSource)
-	end
+    end
 
 end
 
@@ -62,14 +62,14 @@ end
 --[[ Function: Starts Cinemation ]]--
 -------------------------------------
 
-function startCinemation(customCinemationPoint, customCinemationLoop, cinemationBlur)
+function startCinemation(customCinemationPoint, customCinemationLoop, skipCinemationBlur)
 
     if cinemationStatus then return false end
     if customCinemationPoint and type(customCinemationPoint) ~= "table" then return false end
 
     _customCinemationPoint = customCinemationPoint
     _customCinemationLoop = customCinemationLoop
-    if cinemationBlur then
+    if skipCinemationBlur then
         _cinemationBlur = false
     else
         _cinemationBlur = true
@@ -104,5 +104,5 @@ function stopCinemation()
     _cinemationBlur = true
     setCameraTarget(localPlayer)
     return true
-	
+    
 end
--- [Handler]/cinecam_handler/handlers/render.lua
+++ [Handler]/cinecam_handler/handlers/render.lua
@@ -2,8 +2,8 @@
 --[[ Resource: Cinematic Camera Handler
      Script: handlers: render.lua
      Server: -
-	 Author: Tron
-	 Developer: -
+     Author: Tron
+     Developer: -
      DOC: 13/10/2019 (Tron)
      Desc: Cinematic Camera Renderer ]]--
 ----------------------------------------------------------------
@@ -25,15 +25,15 @@ local interpolationTicker = getTickCount()
 
 addEventHandler("onClientPreRender", root, function()
 
-	if interpolationStatus then
-		if (getTickCount() - interpolationTicker) >= interpolationDuration then
-			stopCameraMovement()
-			return false
-		end
-		local posVector_1 = interpolationObjects[1]:getPosition()
-		local posVector_2 = interpolationObjects[2]:getPosition()
-		setCameraMatrix(posVector_1.x, posVector_1.y, posVector_1.z, posVector_2.x, posVector_2.y, posVector_2.z)
-	end
+    if interpolationStatus then
+        if (getTickCount() - interpolationTicker) >= interpolationDuration then
+            stopCameraMovement()
+            return false
+        end
+        local posVector_1 = interpolationObjects[1]:getPosition()
+        local posVector_2 = interpolationObjects[2]:getPosition()
+        setCameraMatrix(posVector_1.x, posVector_1.y, posVector_1.z, posVector_2.x, posVector_2.y, posVector_2.z)
+    end
 
 end)
  
@@ -44,16 +44,16 @@ end)
 
 function stopCameraMovement()
 
-	if not interpolationStatus then return false end
+    if not interpolationStatus then return false end
 
-	interpolationStatus = false
-	for i, j in ipairs(interpolationObjects) do
-		if j and isElement(j) then
-			j:destroy()
-		end
-	end
-	return true
-	
+    interpolationStatus = false
+    for i, j in ipairs(interpolationObjects) do
+        if j and isElement(j) then
+            j:destroy()
+        end
+    end
+    return true
+    
 end
 
 
@@ -66,26 +66,26 @@ function startCameraMovement(cinemationPoint)
     if interpolationStatus then return false end
     if not cinemationPoint or type(cinemationPoint) ~= "table" then return false end
     if not cinemationPoint.cameraStart or type(cinemationPoint.cameraStart) ~= "table" or not cinemationPoint.cameraStartLook or type(cinemationPoint.cameraStartLook) ~= "table" or not cinemationPoint.cameraEnd or type(cinemationPoint.cameraEnd) ~= "table" or not cinemationPoint.cameraEndLook or type(cinemationPoint.cameraEndLook) ~= "table" or not cinemationPoint.cinemationDuration then return false end
-	cameraStartX = tonumber(cinemationPoint.cameraStart.x); cameraStartY = tonumber(cinemationPoint.cameraStart.y); cameraStartZ = tonumber(cinemationPoint.cameraStart.z);
-	cameraStartLookX = tonumber(cinemationPoint.cameraStartLook.x); cameraStartLookY = tonumber(cinemationPoint.cameraStartLook.y); cameraStartLookZ = tonumber(cinemationPoint.cameraStartLook.z);
-	cameraEndX = tonumber(cinemationPoint.cameraEnd.x); cameraEndY = tonumber(cinemationPoint.cameraEnd.y); cameraEndZ = tonumber(cinemationPoint.cameraEnd.z);
-	cameraEndLookX = tonumber(cinemationPoint.cameraEndLook.x); cameraEndLookY = tonumber(cinemationPoint.cameraEndLook.y); cameraEndLookZ = tonumber(cinemationPoint.cameraEndLook.z);
-	cameraDuration = tonumber(cinemationPoint.cinemationDuration);
-	if not cameraStartX or not cameraStartY or not cameraStartZ or not cameraStartLookX or not cameraStartLookY or not cameraStartLookZ or not cameraEndX or not cameraEndY or not cameraEndZ or not cameraEndLookX or not cameraEndLookY or not cameraEndLookZ or not cameraDuration then return false end
+    cameraStartX = tonumber(cinemationPoint.cameraStart.x); cameraStartY = tonumber(cinemationPoint.cameraStart.y); cameraStartZ = tonumber(cinemationPoint.cameraStart.z);
+    cameraStartLookX = tonumber(cinemationPoint.cameraStartLook.x); cameraStartLookY = tonumber(cinemationPoint.cameraStartLook.y); cameraStartLookZ = tonumber(cinemationPoint.cameraStartLook.z);
+    cameraEndX = tonumber(cinemationPoint.cameraEnd.x); cameraEndY = tonumber(cinemationPoint.cameraEnd.y); cameraEndZ = tonumber(cinemationPoint.cameraEnd.z);
+    cameraEndLookX = tonumber(cinemationPoint.cameraEndLook.x); cameraEndLookY = tonumber(cinemationPoint.cameraEndLook.y); cameraEndLookZ = tonumber(cinemationPoint.cameraEndLook.z);
+    cameraDuration = tonumber(cinemationPoint.cinemationDuration);
+    if not cameraStartX or not cameraStartY or not cameraStartZ or not cameraStartLookX or not cameraStartLookY or not cameraStartLookZ or not cameraEndX or not cameraEndY or not cameraEndZ or not cameraEndLookX or not cameraEndLookY or not cameraEndLookZ or not cameraDuration then return false end
 
-	interpolationObjects[1] = Object(1337, cameraStartX, cameraStartY, cameraStartZ)
-	interpolationObjects[2] = Object(1337, cameraStartLookX, cameraStartLookY, cameraStartLookZ)
-	for i, j in ipairs(interpolationObjects) do
-		j:setAlpha(0)
-		j:setScale(0.01)
-		j:setCollisionsEnabled(false)
-	end
-	interpolationObjects[1]:move(cameraDuration, cameraEndX, cameraEndY, cameraEndZ, 0, 0, 0, "InOutQuad")
-	interpolationObjects[2]:move(cameraDuration, cameraEndLookX, cameraEndLookY, cameraEndLookZ, 0, 0, 0, "InOutQuad")
-	interpolationDuration = cameraDuration
-	interpolationTicker = getTickCount()
-	interpolationStatus = true
-	return true
+    interpolationObjects[1] = Object(1337, cameraStartX, cameraStartY, cameraStartZ)
+    interpolationObjects[2] = Object(1337, cameraStartLookX, cameraStartLookY, cameraStartLookZ)
+    for i, j in ipairs(interpolationObjects) do
+        j:setAlpha(0)
+        j:setScale(0.01)
+        j:setCollisionsEnabled(false)
+    end
+    interpolationObjects[1]:move(cameraDuration, cameraEndX, cameraEndY, cameraEndZ, 0, 0, 0, "InOutQuad")
+    interpolationObjects[2]:move(cameraDuration, cameraEndLookX, cameraEndLookY, cameraEndLookZ, 0, 0, 0, "InOutQuad")
+    interpolationDuration = cameraDuration
+    interpolationTicker = getTickCount()
+    interpolationStatus = true
+    return true
 
 end
 
@@ -96,6 +96,6 @@ end
 
 function getInterpolationStatus()
 
-	return interpolationStatus
+    return interpolationStatus
 
 end
