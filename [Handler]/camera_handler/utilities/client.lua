d3d35d61cdaca8089a66988edecd146b526cf841
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-17 01:09:38 +0300

    Updated

---
+++ [Handler]/camera_handler/utilities/client.lua
@@ -0,0 +1,30 @@
+----------------------------------------------------------------
+--[[ Resource: Cinematic Camera Handler
+     Script: settings: client.lua
+     Server: -
+     Author: OvileAmriam
+     Developer: -
+     DOC: 13/10/2019 (OvileAmriam)
+     Desc: Client Sided Utilities ]]--
+----------------------------------------------------------------
+
+
+-----------------
+--[[ Imports ]]--
+-----------------
+
+loadstring(exports.beautify_library:fetchImports())()
+
+
+---------------------------------------------
+--[[ Function: Reverses Cinemation Point ]]--
+---------------------------------------------
+
+function reversecinem(cinemPoint)
+    if not cinemPoint then return false end
+    return {
+        initial = cinemPoint.final,
+        final = cinemPoint.initial,
+        duration = cinemPoint.duration
+    }
+end
