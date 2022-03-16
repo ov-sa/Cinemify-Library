d27959d33a37b56b75c0368eb7ca050491d96ac9
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-17 01:28:17 +0300

    Create camera.lua

---
+++ [Handler]/cinemify_library/utilities/camera.lua
@@ -0,0 +1,16 @@
+----------------------------------------------------------------
+--[[ Resource: Cinemify Library
+     Script: utilities: camera.lua
+     Author: vStudio
+     Developer: -
+     DOC: 13/10/2019
+     Desc: Camera Utilities ]]--
+----------------------------------------------------------------
+
+
+-------------------
+--[[ Variables ]]--
+-------------------
+
+camera = {}
+camera._index = camera
